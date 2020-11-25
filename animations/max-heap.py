633239a0c889a7830382b03712f54fc747a92fd9
unknown <smatintavakolia@gmail.com>
2020-11-25 22:40:49 +0330

    heap insert fixed

--- animations/max-heap.py
+++ animations/max-heap.py
@@ -1,6 +1,7 @@
 from manimlib.imports import *
 import math
 
+
 class Node:
     def __init__(self, index, key, x, y, scaling_factor=0.3, node_color=RED):
         self.key = key
@@ -14,15 +15,17 @@ class Node:
         self.key_obj.move_to([x, y, 0])
 
 
-
 class MaxHeap:
 
     def __init__(self, arr, x, y, hspace=4, vspace=-1, scaling_factor=0.3, node_color=RED):
-        
+
         self.x = x
         self.y = y
         self.size = len(arr)
-        self.height = math.floor(math.log2(self.size))
+        if self.size != 0:
+            self.height = math.floor(math.log2(self.size))
+        else:
+            self.height = 0
 
         self.hspace = hspace
         self.vspace = vspace
@@ -42,8 +45,8 @@ class MaxHeap:
 
             else:
 
-                node_height = math.floor(math.log2(i + 1)) 
-                delta_x = 0.5**node_height * hspace
+                node_height = math.floor(math.log2(i + 1))
+                delta_x = 0.5 ** node_height * hspace
                 node_parent = self.arr[self.parent(i)]
 
                 if self.left(node_parent.index) == i:
@@ -54,17 +57,30 @@ class MaxHeap:
                 node = Node(i, key, node_parent.node_obj.get_x() + delta_x, node_parent.node_obj.get_y() + delta_y)
                 self.nodes.add(node)
 
-                edge = Arrow([node_parent.node_obj.get_x(), node_parent.node_obj.get_y(), 0], [node.node_obj.get_x(), node.node_obj.get_y(), 0])
+                edge = Arrow([node_parent.node_obj.get_x(), node_parent.node_obj.get_y(), 0],
+                             [node.node_obj.get_x(), node.node_obj.get_y(), 0])
                 edge.scale(0.93)
                 self.edges.add(edge)
 
-
             self.arr.append(node)
 
-    def swap(self, i, j):
+    def swap_nodes(self, i, j):
         tmp = self.arr[i]
         self.arr[i] = self.arr[j]
         self.arr[j] = tmp
+
+        child_x = self.arr[i].node_obj.get_x()
+        child_y = self.arr[i].node_obj.get_y()
+
+        self.arr[i].node_obj.set_x(self.arr[j].node_obj.get_x())
+        self.arr[i].node_obj.set_y(self.arr[j].node_obj.get_y())
+        self.arr[i].key_obj.set_x(self.arr[j].key_obj.get_x())
+        self.arr[i].key_obj.set_y(self.arr[j].key_obj.get_y())
+
+        self.arr[j].node_obj.set_x(child_x)
+        self.arr[j].node_obj.set_y(child_y)
+        self.arr[j].key_obj.set_x(child_x)
+        self.arr[j].key_obj.set_y(child_y)
 
     def parent(self, index):
         return (index - 1) // 2
@@ -81,43 +97,47 @@ class MaxHeap:
         else:
             return None
 
-        
-
     def insert(self, key):
 
         if self.size == 0:
-            node = Node(i, key, self.x, self.y)
-            self.nodes.add(node.node_obj)
+            node = Node(0, key, self.x, self.y)
+            self.nodes.add(node)
 
         else:
+            index = self.size
 
-            node_height = math.floor(math.log2(i + 1)) 
-            delta_x = 0.55**node_height * hspace
-            node_parent = self.parent(i)
+            node_height = math.floor(math.log2(index + 1))
+            delta_x = 0.5 ** node_height * self.hspace
+            node_parent = self.parent(index)
 
-            if self.left(node_parent.index) == i:
+            if 2 * node_parent + 1 == index:
                 delta_x *= -1
 
             delta_y = self.vspace
 
-            node = Node(i, key, node_parent.node_obj.get_x() + delta_x, node_parent.node_obj.get_y() + delta_y)
-            self.nodes.add(node.node_obj)
+            node = Node(index, key, self.arr[node_parent].node_obj.get_x() + delta_x,
+                        self.arr[node_parent].node_obj.get_y() + delta_y)
+            self.nodes.add(node)
 
-            edge = Arrow([node_parent.node_obj.get_x(), node_parent.node_obj.get_y(), 0], [node.node_obj.get_x(), node.node_obj.get_y(), 0])
+            edge = Arrow([self.arr[node_parent].node_obj.get_x(), self.arr[node_parent].node_obj.get_y(), 0],
+                         [node.node_obj.get_x(), node.node_obj.get_y(), 0])
             edge.scale(0.93)
             self.edges.add(edge)
+
 
         self.arr.append(node)
 
         self.size = self.size + 1
         self.height = math.floor(math.log2(self.size))
         child_index = self.size - 1
-        parent_index = self.parent(child_index)
-        while self.arr[child_index] > self.arr[parent_index]:
-            self.swap(parent_index, child_index)
-
-            child_index = parent_index
+        if child_index != 0:
             parent_index = self.parent(child_index)
+            while self.arr[child_index].key > self.arr[parent_index].key:
+                self.swap_nodes(parent_index, child_index)
+                child_index = parent_index
+                if child_index == 0:
+                    break
+                parent_index = self.parent(child_index)
 
     def pop(self):
         node = self.arr[0]
@@ -153,20 +173,7 @@ class MaxHeap:
             return
 
         if self.arr[big].key > self.arr[index].key:
-            child_x = self.arr[big].node_obj.get_x()
-            child_y = self.arr[big].node_obj.get_y()
-
-            self.arr[big].node_obj.set_x(self.arr[index].node_obj.get_x())
-            self.arr[big].node_obj.set_y(self.arr[index].node_obj.get_y())
-            self.arr[big].key_obj.set_x(self.arr[index].key_obj.get_x())
-            self.arr[big].key_obj.set_y(self.arr[index].key_obj.get_y())
-
-            self.arr[index].node_obj.set_x(child_x)
-            self.arr[index].node_obj.set_y(child_y)
-            self.arr[index].key_obj.set_x(child_x)
-            self.arr[index].key_obj.set_y(child_y)
-
-            self.swap(big, index)
+            self.swap_nodes(big, index)
             self.heapify(big)
 
     def build(self):
@@ -184,9 +191,7 @@ class MaxHeap:
 
     def print_heap(self):
         for i in range(self.size):
-            print('################')
-            print(self.arr[i])
-            print('################')
+            print(self.arr[i].key)
 
     def sketch_heap(self, scene):
         scene.play(
@@ -200,14 +205,17 @@ class MaxHeap:
 class Intro(Scene):
 
     def construct(self):
-
         # arr = [4, 7, -1, 2, 0, 3, 5]
-        arr = [3, 5, 0, 8, 5, -1, -2, 10, 1]
-        # arr = [10, 8, 0, 5, 5, -1, -2, 3, 1, 2, 4, -3, -1, -4, -6]
-        max_heap = MaxHeap(arr, 2, 2)
-        max_heap.build()
+        # arr = [3, 5, 0, 8, 5, -1, -2, 10, 1]
+        max_heap = MaxHeap([], 2.5, 2)
+        arr = [10, 8, 0, 5, 5, -1, -2, 3, 1, 2, 4, -3, -1, -4, -6]
+        # max_heap.build()
+        for key in arr:
+            max_heap.insert(key)
+        # max_heap.insert(9)
+        # max_heap.insert(10)
+        max_heap.insert(12)
         max_heap.sketch_heap(self)
         max_heap.print_heap()
 
         self.wait(2)
-
