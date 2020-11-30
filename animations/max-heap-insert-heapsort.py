35772abf0c48e587ea6ffa52c308a95c48dbac9a
unknown <smatintavakolia@gmail.com>
2020-12-01 02:36:08 +0330

    creators frame added

--- animations/max-heap-insert-heapsort.py
+++ animations/max-heap-insert-heapsort.py
@@ -1026,6 +1026,27 @@ class HeapSort(Scene):
 
     def construct(self):
 
+        # Introduction
+        title_l1 = TextMobject("Binary Max Heap")
+        title_l2 = TextMobject("Heap Sort")
+        title_l1.scale(1.8)
+        title_l2.scale(1.3)
+        title_l1.shift([0, 0.5, 0])
+        title_l2.shift([0, -0.35, 0])
+        line = Line([-3.8, 0, 0], [3.8, 0, 0])
+        line.set_stroke(WHITE, 1.1, 1)
+        creators = TextMobject("Made by Matin Tavakoli \& Hossein Zaredar")
+        creators.scale(0.4)
+        creators.move_to([5, -3.7, 0])
+        self.add(title_l1)
+        self.add(title_l2)
+        self.add(line)
+        self.wait(2)
+        self.play(Write(creators), run_time=0.7)
+        self.wait(2)
+        self.play(FadeOut(title_l1), FadeOut(title_l2), FadeOut(line))
+        self.wait(1.5)
+
         # title
         title = TextMobject("Heap Sort:")
         title.to_edge(LEFT, buff=0.8)
